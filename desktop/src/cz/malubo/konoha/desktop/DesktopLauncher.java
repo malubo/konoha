6557d19050898f38a646082988c104c157cc91a1
Lubos Maly <malubo@seznam.cz>
2016-07-13 09:40:01 +0200

    initial commit

---
+++ desktop/src/cz/malubo/konoha/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cz.malubo.konoha.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cz.malubo.konoha.Konoha;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Konoha(), config);
+	}
+}
